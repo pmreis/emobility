0a88a40b743d9508dad6f168bf9a88e4ec750091
Pedro Reis <pedrombreis@gmail.com>
2025-04-10 23:15:29 +0100

    changes

--- src/seleniumTest.py
+++ src/seleniumTest.py
@@ -23,7 +23,7 @@ options = [
 for option in options:
     chrome_options.add_argument(option)
 
-driver = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options)
+driver = webdriver.Chrome(executable_path=ChromeDriverManager().install(), options=chrome_options)
 
 driver.execute_cdp_cmd("Page.addScriptToEvaluateOnNewDocument", {
     "source": """
