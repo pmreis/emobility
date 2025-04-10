942295108734d2398dc880b68c491938106cc597
Pedro Reis <pedrombreis@gmail.com>
2025-04-10 19:42:41 +0100

    Change relative path

--- src/mobieSimpleAnalysis.py
+++ src/mobieSimpleAnalysis.py
@@ -1,11 +1,14 @@
 import pandas as pd
 import os.path as osp
+import os
+from pathlib import Path
 
-BASE_DATA_SOURCE_PATH = "./../data/sources"
-BASE_DATA_OUTPUT_PATH = "./../data/outputs"
+BASE_DATA_PATH = Path(
+    os.getenv("DATA_SOURCE_PATH", Path(__file__).resolve().parent.parent / "data")
+)
 
-mobieInPath = osp.normpath(f"{BASE_DATA_SOURCE_PATH}/mobie_locations.json")
-analysisOutPath = osp.normpath(f"{BASE_DATA_OUTPUT_PATH}/simpleAnalysis.csv")
+mobieInPath = osp.normpath(f"{BASE_DATA_PATH}/sources/mobie_locations.json")
+analysisOutPath = osp.normpath(f"{BASE_DATA_PATH}/outputs/simpleAnalysis.csv")
 
 df = pd.read_json(mobieInPath)
 dfnorm = pd.json_normalize(df["data"])
