a6db80acb8b8b5224588931ae2e722ffd5a5f21f
Pedro Reis <pedrombreis@gmail.com>
2025-04-13 23:49:06 +0100

    Changes

--- src/seleniumTest.py
+++ src/seleniumTest.py
@@ -56,7 +56,7 @@ try:
     print(f"implicit wait = {wait}")
     driver.implicitly_wait(wait)
 
-    time.sleep(0.8)
+    time.sleep(0.85)
 
     print("Total requests: " + str(len(driver.requests)))
 
