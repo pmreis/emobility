7fd1febd4b795a34492c55eed72a6b7275f4c4ce
Pedro Reis <pedrombreis@gmail.com>
2025-04-21 00:19:52 +0100

    Improved queries

--- src/db_queries.sql
+++ src/db_queries.sql
@@ -9,21 +9,31 @@ select *
 from Plugs
 where PlugDesign = 'iec60309x2single16';
 
+select *
+from Chargers
+where length(ChargerNapId) > 9;
+
 with recursive
     countAllChargers as (
         select count(1) total
         from Chargers
     ),
-    cte as (
+    cte1 as (
         select
             OperatorMobAbb party_id,
-        count(1) as 'count'
+            count(1) as 'count'
         from Chargers
         group by OperatorMobAbb
         order by count desc
+    ),
+    cte2 as (
+        select
+            row_number() over() idx,
+            cte1.*,
+            cast(cte1.count as real) / cast(countAllChargers.total as real) mrkt_shr
+        from cte1, countAllChargers
     )
 select
-    row_number() over() idx,
-    cte.*,
-    cast(cte.count as real) / cast(countAllChargers.total as real) mrkt_shr
-from cte, countAllChargers;
+    cte2.*,
+    sum(mrkt_shr) over (order by idx) mrkt_shr_acc
+from cte2;
