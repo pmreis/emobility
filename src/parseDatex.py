760d47200fbd479d3b630f0bb388f71da5359a1d
Pedro Reis <pedrombreis@gmail.com>
2025-05-05 19:29:38 +0100

    Ignore empty externalIdentifier

--- src/parseDatex.py
+++ src/parseDatex.py
@@ -78,7 +78,11 @@ def parse_datex(xml_file):
 
         # Parse Charger Plugs data
         for refill_point in site.findall('.//ns6:refillPoint', ns):
-            plug_id = refill_point.get('id') if refill_point.find('.//ns4:externalIdentifier', ns) is None else refill_point.find('.//ns4:externalIdentifier', ns).text.strip()
+            plug_element = refill_point.find('.//ns4:externalIdentifier', ns)
+            if plug_element is None:
+                continue
+
+            plug_id = refill_point.find('.//ns4:externalIdentifier', ns).text.strip()
 
             connector = refill_point.find('.//ns6:connector', ns)
             plug_design = connector.find('.//ns6:connectorType', ns).text
